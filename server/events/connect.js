db0122d49e6f27e638a7032146a6abb76b05d481
张鹏 <zhangpeng@17paipai.cn>
2017-10-09 11:51:35 +0800

    no message

--- server/events/connect.js
+++ server/events/connect.js
@@ -9,30 +9,39 @@ var connect = function (client) {
 };
 
 connect.prototype.before = async function () {
-    // console.log('before connect');
+    // // 获取url参数
     // let query = this.client.handshake.query;
-    // this.name = query.name;
+    // this.query = query;
 };
 
 connect.prototype.do = async function () {
-    // let query = this.client.handshake.query;
     // // 加入房间
+    // this.client.join(this.query.room);
 
-    // this.client.join(query.room);
     // // 房间广播
-    // this.client.to(query.room).broadcast.emit('ll', this.client.id);
+    // this.client.to(this.query.room).broadcast.emit('ll', this.client.id);
+
     // // 存储用户数据
-    // this.client.setUserData('room', query.room);
+    // this.client.setUserData('data', '123456');
+
+    // // 开关用户定时器
     // let f = function (dt) {
     //     console.log('dt', dt);
     // }
     // let id = this.client.schedule(f, 2000);
     // this.client.unschedule(id);
     // this.client.unschedule(f);
+    
+    // // 开关全局定时器
+    // let f = function (dt) {
+    //     console.log('dt', dt);
+    // }
+    // let id = io.schedule(f, 2000);
+    // io.unschedule(id);
+    // io.unschedule(f);
 };
 
 connect.prototype.after = async function () {
     // 读取用户数据
-    // let room = this.client.getUserData('room');
-    // console.log('after connect', room, this.name);
+    // let room = this.client.getUserData('data');
 };
